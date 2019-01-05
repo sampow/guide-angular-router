919635d19d4019aa1e1a30d0f98bc4e2d98bd9b0
Samuel Aminou <sam.niums@gmail.com>
2019-01-05 22:14:07 +0100

    added environment url's api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,20 @@
 export const environment = {
-  production: true
+    production: true,
+    apiReqres: 'https://reqres.in/api/',
+    api: 'https://jsonplaceholder.typicode.com/'
 };
+
+/**
+ *
+ * https://reqres.in/api/
+ * 
+ * Json placholder
+ * https://jsonplaceholder.typicode.com/
+ *
+    posts	100 posts
+    comments	500 comments
+    albums	100 albums
+    photos	5000 photos
+    todos	200 todos
+    users	10 users
+ */
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,26 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiReqres: 'https://reqres.in/api/',
+  api: 'https://jsonplaceholder.typicode.com/'
 };
+
+/**
+ *
+ * https://reqres.in/api/
+ * 
+ * Json placholder
+ * https://jsonplaceholder.typicode.com/
+ *
+    posts	100 posts
+    comments	500 comments
+    albums	100 albums
+    photos	5000 photos
+    todos	200 todos
+    users	10 users
+ */
+
 
 /*
  * For easier debugging in development mode, you can import the following file
