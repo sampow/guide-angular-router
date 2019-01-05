43b4caae1be84678d4c32a8b5f28d7e5cf80b0bf
Samuel Aminou <sam.niums@gmail.com>
2019-01-05 22:14:35 +0100

    added home & blog component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
